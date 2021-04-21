5403393e6d95020d239791d1e7e2bbddff8f8505
Vamshi vemula <vamshivemula444@gmail.com>
2021-04-21 11:53:11 +0530

    New changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-alert("Hey , Guys");
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
